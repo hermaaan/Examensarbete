0c1df54c70d26c536b5beba2f601be4c566292d7
hermaaan <hermansabel23@gmail.com>
2024-11-23 02:11:45 +0100

    mini update

--- DBT/dbt_code_bq/models/dim/dim_date.sql
+++ DBT/dbt_code_bq/models/dim/dim_date.sql
@@ -1,6 +1,6 @@
 WITH src_date as (select * from {{ ref('src_date') }})
 
-SELECT 
+SELECT DISTINCT
     {{dbt_utils.generate_surrogate_key(['date', 'day_name'])}} AS date, --update later
     date AS original_date, -- -ll-
     day_name,
